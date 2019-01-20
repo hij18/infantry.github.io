c9bb0f8489bed30bbf76fd01eed2bd4e415acded
hij18 <46827187+hij18@users.noreply.github.com>
2019-01-20 20:36:57 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -43,8 +43,8 @@ const stuffTable = {
                 const td = document.createElement('td');
                 td.textContent = cellContent;
                 console.log(cellData.getAttribute());
-                console.log(cellData.getAttribute(value));
-                console.log(cellData.getAttribute(ss:Type));
+                console.log(cellData.getAttribute('value'));
+                console.log(cellData.getAttribute('ss:Type'));
                 /*
                 if (cellData[0].attributes[0].value === 'DataTime') {
                     td.type = 'datetime';
