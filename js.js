c97fda4ded4000bb49c75fbc135a95e2071bf0ca
hij18 <46827187+hij18@users.noreply.github.com>
2019-01-21 10:38:00 +0300

    Update js.js

--- js.js
+++ js.js
@@ -89,18 +89,25 @@ const stuffTable = {
         if (this.settings.currentNumOfRow === 500) {
             document.getElementById('next').disabled = true;
         }
+        if (this.settings.currentPage === 1) {
+            return;
+        }
         this.settings.currentNumOfRow += this.settings.rowOnPage;
         this.settings.currentPage++;
         this.changeCurrentPage ();
         this.changeNumberOfPage ();
         this.clearTable ();
         this.renderTable ();
+
     }, 
     
     //Переключает страницы таблицы вперед
     prevNavigationEventHandlers () {
         if (this.settings.currentNumOfRow === 10) {
             document.getElementById('prev').disabled = true;
+        }
+        if (this.settings.currentPage === this.settings.namberOfPage) {
+            return;
         }
         this.settings.currentNumOfRow -= this.settings.rowOnPage;
         this.settings.currentPage--;
